16786bb5af2ad532dd1934010fa00a392a733c3f
emericgautier <emericgtr@gmail.com>
2022-08-09 22:14:16 +0200

    event, for loop, arrays

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-// const generatePassword = () => {};
+// créer 4 tableaux
+const dataLowercase = "azertyuiopmlkjhgfdsqwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "0123456789";
+const dataSymbols = "$^*ùm!:;,&é\"'(-è_ç)";
+const rangeValue = document.getElementById("password-length");
+const passwordOutput = document.getElementById("password-output");
 
+// console.log(passwordOutput);
+
+// const generatePassword = () => {};
 function generatePassword() {
-  console.log("Je génère un mot de passe");
+  let data = [];
+  let password = "";
+
+  if (lowercase.checked) data.push(...dataLowercase);
+  if (uppercase.checked) data.push(...dataUppercase);
+  if (numbers.checked) data.push(...dataNumbers);
+  if (symbols.checked) data.push(...dataSymbols);
+
+  if (data.length === 0) {
+    alert("Veuillez sélectionner des critères");
+    return;
+  }
+
+  for (i = 0; i < rangeValue.value; i++) {
+    password += data[Math.floor(Math.random() * data.length)];
+    console.log(password);
+  } // après la boucle for
+  passwordOutput.value = password;
+
+  passwordOutput.select();
+  document.execCommand("copy");
+
+  generateButton.textContent = "Copié !";
+
+  setTimeout(() => {
+    generateButton.textContent = "Générer mot de passe";
+  }, 2000);
+
+  // console.log("Je génère un mot de passe");
 }
 
 generateButton.addEventListener("click", generatePassword);
